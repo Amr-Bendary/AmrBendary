a49a6c873ae74b244e51b4469118808b22646f89
Amr Bendary <amr.bendary.business@gmail.com>
2022-04-19 06:01:50 +0200

    added the base structure (generated main components and modules)

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlPannelComponent } from './control-pannel/control-pannel.component';
+
+
+
+@NgModule({
+  declarations: [
+    ControlPannelComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
